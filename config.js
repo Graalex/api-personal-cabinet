a0b31df249f6fd01c96458fea70abacdcda0a47b
Graalex <aleksandr.grigorchuk@gmail.com>
2018-03-05 10:02:32 +0200

    Add dotenv

--- config.js
+++ config.js
@@ -1,24 +1,25 @@
 /**
  * config.js -- конфигурация приложения
  */
+require('dotenv').config();
 
 module.exports = {
 	db: {
-		host: '192.168.0.211',
-		port: 3050,
-		database: 'E:/DATA/MARIUPOL.FDB',
-		user: 'SYSDBA',
-		password: 'masterkey',
+		host: process.env.FIREBIRD_BASE_HOST,
+		port: process.env.FIREBIRD_BASE_PORT,
+		database: process.env.FIREBIRD_BASE_DATABASE,
+		user: process.env.FIREBIRD_BASE_USER,
+		password: process.env.FIREBIRD_BASE_PASSWORD,
 		lowercase_keys: false,
 		role: null,
 		pageSize: 4096
 	},
 	security: {
 		appKeys: [
-			{owner: 'ООО АЗОВГАЗ', key: '8SHYVlmUGYMv6ViGRd73'},
-			{owner: 'ПАО Мариупольгаз', key: 'EOhPygF5iyYHjWencvFf'},
+			{owner: 'ООО АЗОВГАЗ', key: process.env.AZOVGAZ_APP_KEY},
+			{owner: 'ПАО Мариупольгаз', key: process.env.MARIUPOLGAZ_APP_KEY},
 		],
-		secret: 'Wow my secret word',
+		secret: process.env.SECRET,
 	},
 	server: {
 		listenPort: 1001,
